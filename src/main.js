8874468fc90e1bf883aed150a8c6eb1d300751d2
Andre Miranda <andy.lm0123@gmail.com>
2023-01-12 15:36:18 -0300

    added form to index.html, started main.js

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// import Swal from 'sweetalert2';
+
+const campoMoeda = document.querySelector('#moeda');
+const btnPesquisa = document.querySelector('#search');
+
+btnPesquisa.addEventListener('click', (event) => {
+    // event.preventDefault();
+});
+
+// const boardGenerator = () => {
+//     for (let x = 1; x <= 5; x += 1) {
+//       const pixelLine = document.createElement('div');
+//       pixelBoard.appendChild(pixelLine);
+  
+//       for (let y = 1; y <= 5; y += 1) {
+//         const pixelCol = document.createElement('div');
+//         pixelCol.className = 'pixel';
+//         pixelLine.appendChild(pixelCol);
+//       }
+//     }
+//   };
