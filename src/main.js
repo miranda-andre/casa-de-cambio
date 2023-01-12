a43c4cc5199a8bef2936bc7a2c86c8db32fefd24
guhpissai <gustavopissai3d@gmail.com>
2023-01-12 18:32:20 -0300

    Finalizando o projeto

--- src/main.js
+++ src/main.js
@@ -2,20 +2,21 @@
 
 const campoMoeda = document.querySelector('#moeda');
 const btnPesquisa = document.querySelector('#search');
+const rates = document.querySelector('#rates')
 
 btnPesquisa.addEventListener('click', (event) => {
-    // event.preventDefault();
-});
+    event.preventDefault();
 
-// const boardGenerator = () => {
-//     for (let x = 1; x <= 5; x += 1) {
-//       const pixelLine = document.createElement('div');
-//       pixelBoard.appendChild(pixelLine);
-  
-//       for (let y = 1; y <= 5; y += 1) {
-//         const pixelCol = document.createElement('div');
-//         pixelCol.className = 'pixel';
-//         pixelLine.appendChild(pixelCol);
-//       }
-//     }
-//   };
+    fetch(`https://api.exchangerate.host/latest?base=${campoMoeda.value}`)
+    .then((response) => response.json())
+    .then((data) => teste(data.rates))
+    const teste = (data) => {
+      const arrayCoins = Object.entries(data);
+      arrayCoins.forEach(coin => {
+        const divCoins = document.createElement('div');
+        divCoins.className = 'coins-values';
+        divCoins.innerHTML = `<img src='./coin-icon.svg'> ${coin[0]}: <span>${coin[1].toFixed(3)}</span>`;
+        rates.appendChild(divCoins);
+      })
+    }
+});
